598d51a3f2923161810d4a5c3f25985715d73d00
ramses93 <ramirezelida11@yahoo.com>
2021-05-31 19:19:39 -0500

    Adding data

---
+++ WeatherPy/Weatherpy.py
@@ -0,0 +1,15 @@
+#Set Dependencies
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import requests
+import time
+import urllib
+import json
+
+#Adding citipy
+from citipy import citipy
+
+#Import API key
+import wkey
---
+++ api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "2eb3254c239c9ed5d96adab55af34838"
+
+# Google API Key
+g_key = "AIzaSyDubACn8znUe4hhiYngcoAbwunpyub84uI"
